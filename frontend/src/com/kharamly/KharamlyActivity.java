a733a97effa6a67ac04c288235475badcd7a0bd3
kamasheto <me@sakr.me>
2011-11-05 18:17:14 +0200

    Added test commit

--- frontend/src/com/kharamly/KharamlyActivity.java
+++ frontend/src/com/kharamly/KharamlyActivity.java
@@ -3,11 +3,14 @@ package com.kharamly;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * @author kamasheto
+ */
 public class KharamlyActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 }
