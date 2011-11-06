bf8c59504d4c7c830b28af72aa6d2e65db85f6b1
kamasheto <me@sakr.me>
2011-11-06 14:39:03 +0200

    Added MapView image

--- frontend/src/com/kharamly/KharamlyActivity.java
+++ frontend/src/com/kharamly/KharamlyActivity.java
@@ -1,16 +1,70 @@
 package com.kharamly;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.*;
 
-/**
- * @author kamasheto
- */
-public class KharamlyActivity extends Activity {
+import android.graphics.drawable.*;
+import android.os.Bundle;
+import com.google.android.maps.*;
+
+// import 
+
+public class KharamlyActivity extends MapActivity {
+	// LinearLayout linearLayout;
+	MapView mapView;
+	
+	List<Overlay> mapOverlays;
+	Drawable drawable;
+	HelloItemizedOverlay itemizedOverlay;
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		mapView = (MapView) findViewById(R.id.mapview);
+		mapView.setBuiltInZoomControls(true);
+		
+		mapOverlays = mapView.getOverlays();
+		drawable = this.getResources().getDrawable(R.drawable.androidmarker);
+		itemizedOverlay = new HelloItemizedOverlay(drawable);
+		
+		GeoPoint point = new GeoPoint(19240000,-99120000);
+		OverlayItem overlayitem = new OverlayItem(point, "", "");
+		
+		GeoPoint point2 = new GeoPoint(35410000, 139460000);
+		OverlayItem overlayitem2 = new OverlayItem(point2, "", "");
+		
+		itemizedOverlay.addOverlay(overlayitem);
+		mapOverlays.add(itemizedOverlay);
+	}
+	
+	@Override
+	protected boolean isRouteDisplayed() {
+	    return false;
+	}
+	
+	
+	private static class HelloItemizedOverlay extends ItemizedOverlay {
+		private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+		
+		public HelloItemizedOverlay(Drawable defaultMarker) {
+			super(boundCenterBottom(defaultMarker));
+			
+		}
+		
+		public void addOverlay(OverlayItem overlay) {
+		    mOverlays.add(overlay);
+		    populate();
+		}
+		
+		@Override
+		protected OverlayItem createItem(int i) {
+		  return mOverlays.get(i);
+		}
+		
+		public int size() {
+			return mOverlays.size();
+		}
 	}
 }
