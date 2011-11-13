538316fa4cb6bd9027b180ae624b3b4b1561904f
kamasheto <me@sakr.me>
2011-11-13 22:10:29 +0200

    Removed misleading comment

--- backend/src/kharamly/settings.py
+++ backend/src/kharamly/settings.py
@@ -12,7 +12,6 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        # please configure the path based on your directory !!!
         'NAME': '../sqlite.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
