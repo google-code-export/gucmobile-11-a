eb315bc6cf7472e12aa43f8000af992164605929
kamasheto <me@sakr.me>
2011-11-15 09:50:35 +0200

    Added routes to mapview based on route returned from django backend!

--- backend/src/kharamly/backend/views.py
+++ backend/src/kharamly/backend/views.py
@@ -8,7 +8,15 @@ import urllib, json
 def api(request, long, lat, who):
     # todo
     # save logs
-    pass
+    # send more routes?
+    response = {"steps" : 
+            [{"s_lng": 31.24906000000001, "s_lat": 30.065440, "e_lng": 31.256110, "e_lat": 30.099050, "col": 1},
+            {"s_lng": 31.256110, "s_lat": 30.099050, "e_lng": 31.255410, "e_lat": 30.105590, "col": 2},
+            {"s_lng": 31.255410, "s_lat": 30.105590, "e_lng": 31.252130, "e_lat": 30.113050, "col": 3},
+            {"s_lng": 31.252130, "s_lat": 30.113050, "e_lng": 31.243610, "e_lat": 30.12307000000001, "col": 120},
+            {"s_lng": 31.243610, "s_lat": 30.12307000000001, "e_lng": 31.236110, "e_lat": 30.132170, "col": 60}]
+    }
+    return HttpResponse(json.dumps(response), mimetype="application/json")
     
     
 
@@ -36,7 +44,3 @@ def getdirections(request, origin, destination, sensor, alternatives):
     url = 'http://maps.googleapis.com/maps/api/directions/json?origin=' + origin + '&destination=' + destination + '&sensor=' + sensor + '&alternatives=' + alternatives
     result = json.load(urllib.urlopen(url))
     return HttpResponse(json.dumps(result), mimetype="application/json")
-
-# Testing playing around with methods in the views file ^k
-# def get_original_param(orig):
-    # return orig.replace('-', '.').replace('_', ',')
--- frontend/src/com/kharamly/KharamlyActivity.java
+++ frontend/src/com/kharamly/KharamlyActivity.java
@@ -1,19 +1,37 @@
 package com.kharamly;
 
+import java.io.*;
+import java.util.*;
+
 import android.content.*;
+import android.graphics.*;
 import android.location.*;
 import android.os.*;
+import android.util.*;
 import android.view.*;
+import android.widget.*;
 import com.google.android.maps.*;
+import org.apache.http.*;
+import org.apache.http.client.*;
+import org.apache.http.client.entity.*;
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.*;
+import org.apache.http.message.*;
+import org.apache.http.params.*;
+import org.json.*;
 
 public class KharamlyActivity extends MapActivity {
-	// LinearLayout linearLayout; // Not used?
 	MapView mapView;
 	MyCustomizedLocationOverlay myLocationOverlay;
+	List<MapRouteOverlay> routeOverlay = new ArrayList<MapRouteOverlay>();
 	
-	// Drawable drawable;
+	private final static String API_URL = "http://10.0.2.2:8000/api/";
+	private final static int TIMEOUT_MILLISEC = 0;
+	private final static String TAG_NAME = "Kharamly";
 	
-	/** Called when the activity is first created. */
+	/** 
+	 * Called when the activity is first created. 
+	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -23,16 +41,18 @@ public class KharamlyActivity extends MapActivity {
 		mapView.setBuiltInZoomControls(true);
 		
 		myLocationOverlay = new MyCustomizedLocationOverlay(this, mapView);
+		
+		mapView.getOverlays().add(myLocationOverlay);
 		myLocationOverlay.enableMyLocation();
+        
 		myLocationOverlay.runOnFirstFix(new Runnable() {
 			public void run() {
+			    Log.e(TAG_NAME, "RUNNING ON FIRST FIX");
 				mapView.getController().animateTo(myLocationOverlay.getMyLocation());
 			}
 		});
 		
-		mapView.getOverlays().add(myLocationOverlay);
 		mapView.postInvalidate();
-		// drawable = this.getResources().getDrawable(R.drawable.androidmarker);
 	}
 	
 	@Override
@@ -58,20 +78,120 @@ public class KharamlyActivity extends MapActivity {
 	    return true;
 	}
 	
-	public static class MyCustomizedLocationOverlay extends MyLocationOverlay {
+	public static void background(final Runnable r) {
+		new Thread() {
+			public void run() {
+				r.run();
+			}
+		}.start();
+	}
+	
+	/**
+	 * Source: http://www.codeproject.com/KB/android/jsonandroidphp.aspx
+	 */
+    public static String convertStreamToString(InputStream is) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        while ((line = reader.readLine()) != null)
+            sb.append(line + "\n");                
+        is.close();
+        return sb.toString();
+    }
+	
+	public class MyCustomizedLocationOverlay extends MyLocationOverlay {
+	    MapView mapView;
+	    
 		public MyCustomizedLocationOverlay(Context context, MapView mapView) {
 			super(context, mapView);
+			this.mapView = mapView;
+			Log.e(TAG_NAME, "STARTED LOCATION LISTENER");
 		}
 		
 		@Override
 		public void onLocationChanged(Location location) {
-		    // let's pretend for now we'll update the routes everytime the location is changed
+		    Log.e(TAG_NAME, "LOCATION CHANGED");
+		    
+		    /**
+		     * Remove any old route info
+		     */
+		    for (MapRouteOverlay o : KharamlyActivity.this.routeOverlay) {
+		        mapView.getOverlays().remove(o);
+		    }
+		    routeOverlay.clear();
+		    
+		    // Let's pretend for now we'll update the routes everytime the location is changed 
 		    // this might drain the battery, but we'll see!
 		    
-		    // todo's
-		    // ping server
-		    // get response
-		    // render response on map
+            // Source: http://www.codeproject.com/KB/android/jsonandroidphp.aspx
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpGet httpget = new HttpGet(API_URL + 
+                                        location.getLongitude() + "/" + 
+                                        location.getLatitude() + "/" + 
+                                        Installation.id(KharamlyActivity.this));
+            try {
+                Log.i(getClass().getSimpleName(), "send  task - start");
+                HttpResponse httpresponse = httpclient.execute(httpget);
+                String responseBody = convertStreamToString(httpresponse.getEntity().getContent());
+                
+                JSONObject json = new JSONObject(responseBody);
+                JSONArray jArray = json.getJSONArray("steps");
+                
+                ArrayList<HashMap<String, Integer>> mylist = new ArrayList<HashMap<String, Integer>>();
+
+                for (int i = 0; i < jArray.length(); i++) {
+                    JSONObject e = jArray.getJSONObject(i);
+
+                    HashMap<String, Integer> map = new HashMap<String, Integer>();
+                    map.put("s_lng", (int) (e.getDouble("s_lng") * 1E6));
+                    map.put("s_lat", (int) (e.getDouble("s_lat") * 1E6));
+                    map.put("e_lng", (int) (e.getDouble("e_lng") * 1E6));
+                    map.put("e_lat", (int) (e.getDouble("e_lat") * 1E6));
+                    map.put("col", e.getInt("col"));
+
+                    mylist.add(map);
+                }
+                
+                // Process mylist (list of hashmaps) to show on map
+                List<Overlay> overlays = mapView.getOverlays();
+                for (HashMap<String, Integer> step : mylist) {
+                    overlays.add(new MapRouteOverlay(new GeoPoint(step.get("s_lat"), step.get("s_lng")),
+                                                    new GeoPoint(step.get("e_lat"), step.get("e_lng")),
+                                                    step.get("col")));
+                }
+                
+                mapView.invalidate();
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 		}
+	}
+	
+	public class MapRouteOverlay extends Overlay {
+        private GeoPoint gp1;
+        private GeoPoint gp2;
+        private int color;
+
+        public MapRouteOverlay(GeoPoint gp1, GeoPoint gp2, int color) {
+            this.gp1 = gp1;
+            this.gp2 = gp2;
+            this.color = color;
+        }
+        
+        public void draw(Canvas canvas, MapView mapView, boolean shadow) {
+            Projection projection = mapView.getProjection();
+            Paint paint = new Paint();
+            Point point = new Point();
+            projection.toPixels(gp1, point);
+            paint.setColor(color);
+            Point point2 = new Point();
+            projection.toPixels(gp2, point2);
+            paint.setStrokeWidth(5);
+            paint.setAlpha(120);
+            canvas.drawLine(point.x, point.y, point2.x, point2.y, paint);
+            super.draw(canvas, mapView, shadow);
+        }
 	}
 }
