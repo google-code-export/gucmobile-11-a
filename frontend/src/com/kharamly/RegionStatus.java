be5e81470912f9bca12fba5d2b38a3ec15a6e14f
ahmed abouraya <ahmed.abourayax@gmail.com>
2011-12-23 18:20:01 +0100

    RegionStatus class contacts the server to get all steps statuses in a certain region

---
+++ frontend/src/com/kharamly/RegionStatus.java
@@ -0,0 +1,66 @@
+package com.kharamly;
+
+import java.util.ArrayList;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.maps.GeoPoint;
+
+public class RegionStatus {
+	static String respStr;
+
+	public static ArrayList<Step> getRegionStatus(double longitude,
+			double latitude,double radius, Context c, TextView txtMsg) {
+
+		String urlString = "http://10.0.2.2:8000/inRadius/" + longitude + "/"
+				+ latitude + "/"+radius+"/";
+
+		Log.d("URL", urlString);
+		ArrayList<Step> steps = new ArrayList<Step>();
+		try {
+
+			DefaultHttpClient CLIENT = new DefaultHttpClient();
+
+			HttpResponse resp = CLIENT.execute(new HttpGet(urlString));
+			respStr = EntityUtils.toString(resp.getEntity());
+			JSONArray jsonArray = new JSONArray(respStr);
+			for (int i = 0; i < jsonArray.length(); i++) {
+				JSONObject currentStep = jsonArray.getJSONObject(i);
+				
+				double[] start = {
+						Double.parseDouble(currentStep.getString("start_location_longitude")),
+						Double.parseDouble(currentStep.getString("start_location_latitude")) };
+
+				double[] end = {
+						Double.parseDouble(currentStep.getString("end_location_longitude")),
+						Double.parseDouble(currentStep.getString("end_location_latitude")) };
+
+				Step cS = new Step();
+				cS.source = new GeoPoint((int) (start[1] * 1E6),
+						(int) (start[0] * 1E6));
+				cS.destination = new GeoPoint((int) (end[1] * 1E6),
+						(int) (end[0] * 1E6));
+				cS.speed = currentStep.getDouble("avg_speed");
+				steps.add(cS);
+
+			}
+
+		} catch (Exception e) {
+
+			Toast.makeText(c, "exception" + e.toString(), Toast.LENGTH_SHORT)
+					.show();
+			//txtMsg.append(e.toString() + "\n");
+		}
+		return steps;
+	}
+}
