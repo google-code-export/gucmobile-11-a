cf1af2ed48802972223fc20a5a24d0e2ed14b797
kamasheto <local@machine>
2011-12-26 00:04:48 +0200

    Changed domain to live site

--- frontend/src/com/kharamly/Cons.java
+++ frontend/src/com/kharamly/Cons.java
@@ -1,6 +1,6 @@
 package com.kharamly;
 
 public final class Cons {
-	public final static String SERVER_URL = "http://10.0.2.2:8000/"; //"http://kharamly.alwaysdata.net/";
+	public final static String SERVER_URL = "http://kharamly.alwaysdata.net/";
 	public final static String API_URL = SERVER_URL + "api/";
 }
